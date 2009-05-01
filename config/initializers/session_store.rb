c3731f03368d1cb79cace2c5d935957378b2cff1
faithfulgeek <joe@faithfulgeek.org>
2009-04-30 21:02:04 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,2 @@
+ActionController::Base.session = { :session_key => '_XDAPRK_session', :secret => 'LOWJAFYVFGOEBMUCXCDSJHTSDLHZSATRZTAFUNIZ' }
+ActionController::Base.session_store = :active_record_store
